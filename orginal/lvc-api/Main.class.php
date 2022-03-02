f6e4c98028218eaed35c70e7fd6c48ea79d7d52c
ChaosSchwein | Shaun <shaun.luedeke5520@gmail.com>
2022-03-02 08:24:47 +0100

    Bug fixes

--- orginal/lvc-api/Main.class.php
+++ orginal/lvc-api/Main.class.php
@@ -886,12 +886,6 @@ class ad{
     private function is_url($uri):bool{
         return(preg_match( '/^(http|https):\\/\\/[a-z0-9_]+([\\-\\.]{1}[a-z_0-9]+)*\\.[_a-z]{2,5}'.'((:[0-9]{1,5})?\\/.*)?$/i' ,$uri));
     }
-    private function isFileImage($file): bool
-    {
-        $result = exif_imagetype($file);
-
-        return is_int($result) && $result >= 1 && $result <= 18;
-    }
 }
 
 class scanDir
